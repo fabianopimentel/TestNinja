625bea88c65dae82e8b4e608a8a3141fd244c7a0
Fabiano Pimentel <fabiano.pimentel@benner.com.br>
2018-03-07 23:09:03 -0300

    Testing another mock of VideoService, using a repository and isolating external resource... :)

--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -14,12 +14,14 @@ namespace TestNinja.UnitTests.Mocking
     {
         private VideoService _videoService;
         private Mock<IFileReader> _fileReader;
+        private Mock<IVideoRepository> _repository;
 
         [SetUp]
         public void SetUp()
         {
             _fileReader = new Mock<IFileReader>();
-            _videoService = new VideoService(_fileReader.Object);
+            _repository = new Mock<IVideoRepository>();
+            _videoService = new VideoService(_fileReader.Object, _repository.Object);
         }
 
         [Test]
@@ -33,9 +35,28 @@ namespace TestNinja.UnitTests.Mocking
         }
 
         [Test]
-        public void GetUnprocessedVideosAsCsv_WhenCalled_ReturnTwoVideosIdSeparatedByComma()
+        public void GetUnprocessedVideosAsCsv_AllVideosAreProcessed_ReturnAnEmptyString()
         {
-            
+            _repository.Setup(r => r.GetUnprocessedVideos()).Returns(new List<Video>());
+
+            var result = _videoService.GetUnprocessedVideosAsCsv();
+
+            Assert.That(result, Is.EqualTo(string.Empty));
+        }
+
+        [Test]
+        public void GetUnprocessedVideosAsCsv_AFewUnprocessedVideos_ReturnAStringWithIdOfUnprocessedVideos()
+        {
+            _repository.Setup(r => r.GetUnprocessedVideos()).Returns(new List<Video>
+            {
+                new Video{Id = 1},
+                new Video{Id = 2},
+                new Video{Id = 3},
+            });
+
+            var result = _videoService.GetUnprocessedVideosAsCsv();
+
+            Assert.That(result, Is.EqualTo("1,2,3"));
         }
     }
 }
